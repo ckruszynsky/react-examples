22782c19255caab11c705ec7d014056c459b398f
Chad Kruszynsky <ckruszynsky@turner-industries.com>
2017-02-15 15:07:17 -0600

    added switch example

--- react-recipes/src/App.js
+++ react-recipes/src/App.js
@@ -10,8 +10,8 @@ class App extends Component {
         <div className="App App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to React</h2>
-        </div>
-        <Switch />
+        </div>   
+        <Switch />             
       </div>
     );
   }
